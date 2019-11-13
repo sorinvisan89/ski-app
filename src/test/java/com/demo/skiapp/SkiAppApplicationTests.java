f43fb77187a7a7f8e40fb9998a2c5117017ab68a
VIS73600 <Sorin.Visan@viavisolutions.com>
2019-11-13 20:37:05 +0200

    initial commit

---
+++ src/test/java/com/demo/skiapp/SkiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.skiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
